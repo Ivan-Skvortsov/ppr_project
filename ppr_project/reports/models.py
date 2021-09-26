ca55cf834be365fac1a2641ba9fac97748ac7863
Ivan Skvortsov <pprofcheg@gmail.com>
2021-09-26 18:06:02 +0300

    started work on models

--- ppr_project/reports/models.py
+++ ppr_project/reports/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+
+class Employee(models.Model):
+    name = CharField(max_length=30, null=False, blank=False)
+    position = CharField(max_length=20, null=False, blank=False)
+    department = CharField(max_length=15, null=False, blan=False)
+
+
+# class Equipment(models.Model):
+    
+
+# class Facility(models.Model):
+#     name = CharField(max_length=15, null=False, blank=True, unique=True)
+#     equipment_type = CharField(max_length=10, null=False, blank=False)
+#     equipment_type_maintenance_period = 
--- ppr_project/reports/views.py
+++ ppr_project/reports/views.py
@@ -3,7 +3,4 @@ from django.shortcuts import render
 
 def index(request):
     template = 'index.html'
-    context = {
-        'key': '123'
-    }
-    return render(request, template, context)
+    return render(request, template)
