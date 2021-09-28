782e023baeec22742726ecad9208e37dca2cb8dc
Ivan Skvortsov <pprofcheg@gmail.com>
2021-09-28 13:06:12 +0300

    modified template's DIR

--- ppr_project/ppr_project/settings.py
+++ ppr_project/ppr_project/settings.py
@@ -53,11 +53,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ppr_project.urls'
 
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+# TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
