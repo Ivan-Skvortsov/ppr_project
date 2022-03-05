85d69c0634996420473e692fc00ae7d9cc02b4a6
Ivan Skvortsov <pprofcheg@gmail.com>
2022-03-05 23:56:12 +0300

    added custom error pages

--- ppr_project/ppr_project/urls.py
+++ ppr_project/ppr_project/urls.py
@@ -11,6 +11,10 @@ urlpatterns = [
     path('ppradmin/', admin.site.urls),
 ]
 
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+handler403 = 'core.views.permission_denied'
+
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
