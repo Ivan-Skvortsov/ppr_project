4e637d12b382c6d6b35b92fd81e574ecf4d5270a
Ivan Skvortsov <pprofcheg@gmail.com>
2022-09-26 16:40:22 +0300

    pre-commit-test

--- ppr_project/api/serializers.py
+++ ppr_project/api/serializers.py
@@ -1,19 +1,15 @@
+from reports.models import Employee, Schedule
 from rest_framework import serializers
 
 
-from reports.models import Employee, Schedule
-
-
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
-
     class Meta:
         model = Employee
         # fields = ['name', 'position', 'department']
-        fields = '__all__'
+        fields = "__all__"
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Schedule
-        fields = '__all__'
+        fields = "__all__"
