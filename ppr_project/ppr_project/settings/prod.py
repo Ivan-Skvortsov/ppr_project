998e8c6cc39a3026171f3e41c5761608c900434c
Ivan Skvortsov <pprofcheg@gmail.com>
2022-10-18 18:19:21 +0300

    added csrf_trusted_origins

--- ppr_project/ppr_project/settings/prod.py
+++ ppr_project/ppr_project/settings/prod.py
@@ -6,6 +6,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 DEBUG = False
 
 ALLOWED_HOSTS = ['ks45.online', 'www.ks45.online', 'localhost', 'web']
+CSRF_TRUSTED_ORIGINS = ['https://*.ks45.online']
 
 DATABASES = {
     'default': {
